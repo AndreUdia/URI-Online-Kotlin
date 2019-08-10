dd118f8db465065c44afe56dd146d4aa301581ce
cabomaldade <soutom@yahoo.com.br>
2019-08-10 12:45:51 -0300

    1014 done!

---
+++ 1014.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+
+    val km: Int = reader.nextInt()
+    val fuelSpent: Double = reader.next().toDouble()
+
+    fun consumptionPerLiters (kms: Int, fuelSpent: Double) : Double {
+        return kms / fuelSpent
+    }
+
+    println("%.3f".format(consumptionPerLiters(km, fuelSpent)).replace(",",".") + " km/l")
+}
