108f0e30d007badf8e5615aa53093c9e93311ad9
cabomaldade <soutom@yahoo.com.br>
2019-08-11 11:33:59 -0300

    done!

--- 1013.kt
+++ 1013.kt
@@ -9,7 +9,7 @@ fun main(args: Array<String>) {
 
     val reader = Scanner(System.`in`)
 
-    var list = listOf<Int>(reader.nextInt(),reader.nextInt(),reader.nextInt())
+    val list = listOf(reader.nextInt(),reader.nextInt(),reader.nextInt())
 
     println(list.maxBy { it.absoluteValue }.toString() + " eh o maior")
 
