7de4a0b5a1f505546fb5101aae8215ad7feb1126
cabomaldade <soutom@yahoo.com.br>
2019-08-11 22:33:49 -0300

    done! Logic Used - maps [key].times

---
+++ 1038.kt
@@ -0,0 +1,21 @@
+/*
+    Created by Cabomaldade - André Vasconcelos Souto
+ */
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+
+    val productCode = reader.nextInt()
+    val quantity = reader.nextInt()
+
+    val products = mapOf(1 to 4.00, 2 to 4.50, 3 to 5.00, 4 to 2.00, 5 to 1.50)
+
+    for(key in products.keys){
+        if(productCode == key){
+            println("Total: R$ %.2f".format(products[key]?.times(quantity)))
+        }
+    }
+}
