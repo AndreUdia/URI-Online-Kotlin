00c977d6630c6bf7b22b51ceded64c782dbb65b7
cabomaldade <soutom@yahoo.com.br>
2019-08-13 21:18:08 -0300

    done! Logic Used - mafOf

---
+++ 1044.kt
@@ -0,0 +1,23 @@
+/*
+    Created by Cabomaldade - André Vasconcelos Souto
+ */
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+    reader.useLocale(Locale.ENGLISH)
+
+    val a = reader.nextInt()
+    val b = reader.nextInt()
+
+    val c = maxOf(a,b)
+
+    if(c % a == 0 && c % b == 0){
+        println("Sao Multiplos")
+    } else{
+        println("Nao sao Multiplos")
+    }
+
+}
