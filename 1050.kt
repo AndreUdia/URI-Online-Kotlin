1bbb5e1a5c2f1196b729c0a9289e1edcd0aeadc9
cabomaldade <soutom@yahoo.com.br>
2019-11-09 18:40:33 -0300

    done! Logic Used - mapOf

---
+++ 1050.kt
@@ -0,0 +1,32 @@
+/*
+    Created by Cabomaldade - André Vasconcelos Souto
+    Tecnólogo em Sistemas Para Internet - IFTM - Uberlândia - MG
+ */
+
+import java.util.*
+
+fun main() {
+
+    val reader = Scanner(System.`in`)
+    reader.useLocale(Locale.ENGLISH)
+
+    val ddd = reader.nextInt()
+
+    val listOfDDDs = mapOf(
+        61 to "Brasilia",
+        71 to "Salvador",
+        11 to "Sao Paulo",
+        21 to "Rio de Janeiro",
+        32 to "Juiz de Fora",
+        19 to "Campinas",
+        27 to "Vitoria",
+        31 to "Belo Horizonte"
+    )
+    if(listOfDDDs.containsKey(ddd)){
+        println(listOfDDDs.get(ddd))
+    }
+    else{
+        println("DDD nao cadastrado")
+    }
+}
+
