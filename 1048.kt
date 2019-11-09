4c35d01d37261d05ccee557afee8ba601620b936
cabomaldade <soutom@yahoo.com.br>
2019-11-09 08:18:43 -0300

    done! Logic Used - when statement - range - String.format - replace

---
+++ 1048.kt
@@ -0,0 +1,49 @@
+/*
+    Created by Cabomaldade - André Vasconcelos Souto
+    Tecnólogo em Sistemas Para Internet - IFTM - Uberlândia - MG
+ */
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+    reader.useLocale(Locale.ENGLISH)
+
+    val oldSalary = reader.nextDouble()
+
+    val percent: Int
+    val newSalary: Double
+
+    when{
+        oldSalary in 0.00..400.00 -> {
+            newSalary = oldSalary + (oldSalary * 15 / 100)
+            percent = 15
+        }
+        oldSalary in 400.01..800.00 -> {
+            newSalary = oldSalary + (oldSalary * 12 / 100)
+            percent = 12
+        }
+        oldSalary in 800.01..1200.00 -> {
+            newSalary = oldSalary + (oldSalary * 10 / 100)
+            percent = 10
+        }
+        oldSalary in 1200.01..2000.00 -> {
+            newSalary = oldSalary + (oldSalary * 7 / 100)
+            percent = 7
+        }
+        else -> {
+            newSalary = oldSalary + (oldSalary * 4 / 100)
+            percent = 4
+        }
+    }
+
+    val readjustment = (newSalary - oldSalary)
+
+    val readjustmentGained = String.format("%.2f", readjustment).replace(",",".")
+
+    println("Novo salario: " + String.format("%.2f", newSalary).replace(",","."))
+    println("Reajuste ganho: $readjustmentGained")
+    println("Em percentual: $percent %")
+
+}
