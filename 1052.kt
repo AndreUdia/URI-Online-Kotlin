7b97670cc4dbd4494a2299ff10036a343935501c
cabomaldade <soutom@yahoo.com.br>
2019-11-10 11:48:52 -0300

    done! Logic Used - mapOf

---
+++ 1052.kt
@@ -0,0 +1,30 @@
+/*
+    Created by Cabomaldade - André Vasconcelos Souto
+    Tecnólogo em Sistemas Para Internet - IFTM - Uberlândia - MG
+ */
+
+import java.util.*
+
+fun main() {
+
+    val reader = Scanner(System.`in`)
+    reader.useLocale(Locale.ENGLISH)
+
+    val code = reader.nextInt()
+
+    val listOfMonths = mapOf(
+        1 to "January",
+        2 to "February",
+        3 to "March",
+        4 to "April",
+        5 to "May",
+        6 to "June",
+        7 to "July",
+        8 to "August",
+        9 to "September",
+        10 to "October",
+        11 to "November",
+        12 to "December"
+    )
+    println(listOfMonths[code])
+}
