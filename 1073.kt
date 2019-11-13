3e07b420f421d369ed0ff5962a261a382896cd5a
cabomaldade <soutom@yahoo.com.br>
2019-11-13 20:30:08 -0300

    done! Logic Used - if range

---
+++ 1073.kt
@@ -0,0 +1,19 @@
+/*
+    Created by Cabomaldade - André Vasconcelos Souto
+    Tecnólogo em Sistemas Para Internet - IFTM - Uberlândia - MG
+ */
+
+import java.util.*
+
+fun main(args: Array<String>) {
+
+    val reader = Scanner(System.`in`)
+
+    val numberLimit = reader.nextInt()
+
+    for(i in 1..numberLimit){
+        if(i % 2 == 0){
+            println("$i^2 = ${i * i}")
+        }
+    }
+}
